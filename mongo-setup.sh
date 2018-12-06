0ebeda5b7edcc1f2cddfc86011c46f2f8e6bf43e
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-12-07 04:10:25 +0700

    Fix Fail to Setup Mongos

--- mongo-setup.sh
+++ mongo-setup.sh
@@ -15,6 +15,7 @@ echo "[setup mongo-shard-3]" &&
 docker exec -it mongo-shard-3 bash -c "echo 'rs.initiate({_id:\"shard-3\",members:[{_id:0,host:\"mongo-shard-3:27017\"}]})' | mongo" &&
 
 echo "[setup mongos]" &&
+sleep 10s &&
 docker exec -it mongos bash -c "echo 'sh.addShard(\"shard-1/mongo-shard-1:27017\")' | mongo" &&
 docker exec -it mongos bash -c "echo 'sh.addShard(\"shard-2/mongo-shard-2:27017\")' | mongo" &&
 docker exec -it mongos bash -c "echo 'sh.addShard(\"shard-3/mongo-shard-3:27017\")' | mongo"
